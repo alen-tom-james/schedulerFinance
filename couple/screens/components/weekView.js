73cea4f5e4db4e294b20c517c1d5121457fe48ba
alen-tom-james <145158385+alen-tom-james@users.noreply.github.com>
2023-10-16 11:06:54 -0400

    huge week view improvements

--- couple/screens/components/weekView.js
+++ couple/screens/components/weekView.js
@@ -1,8 +1,11 @@
 import React, { useState, useContext, useRef, useEffect } from 'react';
 import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Dimensions } from 'react-native';
-import { DateContext } from '../../App';  // Ensure the path is correct
+import { DateContext } from '../../App';  
 
-const { width: SCREEN_WIDTH } = Dimensions.get('window');
+const TOTAL_SCREEN_WIDTH = Dimensions.get('window').width;
+const TIME_COLUMN_WIDTH = 40;  // Adjusted width for the time column
+const screenW = TOTAL_SCREEN_WIDTH - TIME_COLUMN_WIDTH;
+const WEEK_VIEW_WIDTH = (TOTAL_SCREEN_WIDTH - TIME_COLUMN_WIDTH) ;// Adjusted to take up the rest of the screen
 
 const WeekView = () => {
   const { selectedDate, setSelectedDate } = useContext(DateContext);
@@ -17,18 +20,26 @@ const WeekView = () => {
 
   const [currentWeek, setCurrentWeek] = useState(getWeek(selectedDate));
   const scrollViewRef = useRef(null);
+  const verticalScrollRef = useRef(null);
 
   useEffect(() => {
-    scrollViewRef.current.scrollTo({ x: SCREEN_WIDTH, animated: false });
+    scrollViewRef.current.scrollTo({ x: WEEK_VIEW_WIDTH, animated: false });
   }, [currentWeek]);
 
   const handleScroll = (event) => {
     const offsetX = event.nativeEvent.contentOffset.x;
-    if (offsetX > SCREEN_WIDTH) {
+    if (offsetX > WEEK_VIEW_WIDTH) {
       setCurrentWeek(prevWeek => getWeek(new Date(prevWeek.start.setDate(prevWeek.start.getDate() + 7))));
-    } else if (offsetX < SCREEN_WIDTH) {
+    } else if (offsetX < WEEK_VIEW_WIDTH) {
       setCurrentWeek(prevWeek => getWeek(new Date(prevWeek.start.setDate(prevWeek.start.getDate() - 7))));
     }
+  };
+
+  const handleVerticalScroll = (event) => {
+    verticalScrollRef.current.scrollTo({
+      y: event.nativeEvent.contentOffset.y,
+      animated: false,
+    });
   };
 
   const renderWeek = (weekOffset) => {
@@ -53,29 +64,54 @@ const WeekView = () => {
     );
   };
 
+  const renderTimeColumn = () => {
+    return (
+      <ScrollView 
+        ref={verticalScrollRef}
+        style={styles.timeColumn}
+        scrollEventThrottle={16}
+        onScroll={handleVerticalScroll}
+      >
+        {Array.from({ length: 24 }, (_, i) => (
+          <Text key={i} style={styles.timeLabel}>{`${i}:00`}</Text>
+        ))}
+      </ScrollView>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      {/* Header Row */}
       <View style={styles.headerRow}>
         {['S', 'M', 'T', 'W', 'T', 'F', 'S'].map((day, index) => (
           <Text key={index} style={styles.dayHeader}>{day}</Text>
         ))}
       </View>
-      {/* Week View content */}
-      <ScrollView
-        ref={scrollViewRef}
-        horizontal
-        pagingEnabled
-        onMomentumScrollEnd={handleScroll}
-        showsHorizontalScrollIndicator={false}
-        contentOffset={{ x: SCREEN_WIDTH }}
-      >
-        {[-1, 0, 1].map((weekOffset) => (
-          <View style={{ width: SCREEN_WIDTH }} key={weekOffset}>
-            {renderWeek(weekOffset)}
-          </View>
-        ))}
-      </ScrollView>
+      <View style={styles.content}>
+        {renderTimeColumn()}
+        <ScrollView
+          ref={scrollViewRef}
+          horizontal
+          pagingEnabled
+          onMomentumScrollEnd={handleScroll}
+          showsHorizontalScrollIndicator={false}
+          contentOffset={{ x: WEEK_VIEW_WIDTH }}
+          style={styles.weekScrollView}
+        >
+          {[-1, 0, 1].map((weekOffset) => (
+            <View style={{ width: WEEK_VIEW_WIDTH}}>
+              <ScrollView
+                key={weekOffset}
+                onScroll={handleVerticalScroll}
+                scrollEventThrottle={16}
+                style={styles.weekY}
+              >
+                {renderWeek(weekOffset)}
+              </ScrollView>
+              
+            </View>
+          ))}
+        </ScrollView>
+      </View>
     </View>
   );
 };
@@ -83,52 +119,79 @@ const WeekView = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
   },
   headerRow: {
     flexDirection: 'row',
+    width: WEEK_VIEW_WIDTH + TIME_COLUMN_WIDTH,  // Adjusted here
     marginBottom: 5,
     backgroundColor: '#4b9cd3',
-    borderRadius: 10,
-    marginTop: 50,
-    margin: 22,
-    left: 18
+    borderTopRightRadius: 25,
+    borderTopLeftRadius: 25,
+    padding: 10,
   },
   dayHeader: {
-    flex: 1,
-    textAlign: 'center',
+    width: WEEK_VIEW_WIDTH / 7,
     fontWeight: 'bold',
-    fontSize: 25,
+    fontSize: 18,
     color: 'white',
-    padding: 6,
+    textAlign: 'center',
   },
   weekContainer: {
     flexDirection: 'row',
-    marginBottom: 5,
-    paddingLeft: 40,
-    paddingRight: 20
+    width: WEEK_VIEW_WIDTH,  // Make sure the weekContainer is also taking the full width
+    position:'absolute',
+
   },
   dayCell: {
-    width: `${100/7}%`,
-    paddingTop: 0,
-    paddingBottom: 4,
+    width: WEEK_VIEW_WIDTH / 7,  // Adjusted here
+    paddingTop: 10,
+    paddingBottom: 10,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'rgba(75, 156, 211, 0.2)',
     borderRadius: 5,
     margin: 1,
-    marginBottom: 5,
   },
   dayText: {
-    fontSize: 20,
-    color: '#4b9cd3',
+    fontSize: 16,
+    color: 'white',
   },
   selectedDayText: {
-    fontSize: 30,
+    fontSize: 18,
     color: 'blue',
-    opacity: '.45'
+  },
+  content: {
+    flexDirection: 'row',
+    marginLeft: -300,
+    left: 150
+  },
+  timeColumn: {
+    width: TIME_COLUMN_WIDTH,
+    marginRight: 5,
+    right: 170,
+    zIndex: 5,
+    borderRightWidth: 1,
+    borderRightColor: '#ddd',
+  },
+  timeLabel: {
+    height: 60,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    borderBottomWidth: 0.5,
+    borderBottomColor: '#ddd',
+    fontSize: 12,
+    color: 'white', 
+    marginLeft: 320,
+    
+  },
+  weekScrollView: {
+    flex: 1,
+    width: WEEK_VIEW_WIDTH,
+    right: 150,
   },
 });
+
 
 export default WeekView;
